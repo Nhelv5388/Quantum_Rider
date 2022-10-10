f4aa06f4ded8c0c537769a741753938636095300
sekiRyoma <ryo-ma0110@docomo.ne.jp>
2022-10-10 23:44:41 +0900

    プレイヤーの挙動試し

---
+++ Quantum_Rider/Assets/Member/Seki/Scripts/Uku.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uku : MonoBehaviour
+{
+    [SerializeField]
+    GameObject rayStart;
+    [SerializeField]
+    float hoverMax = 0,hoverPower=1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Hover();
+        }
+    }
+    
+
+    private void Hover()
+    {
+        Ray ray = new Ray(rayStart.transform.position, -Vector3.forward);
+        //ÉåÉCÇÃèoÇ∑èÍèäÇ∆å¸Ç´
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, hoverMax);
+        //Ç»Ç™Ç≥Ç∆Ç©ÅH
+        
+        if (hit.collider)
+        {
+            Debug.Log(hit.collider.gameObject.name);
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(0, hoverPower, 0));
+        }
+        Debug.DrawRay(ray.origin, ray.direction * hoverMax, Color.green, 5, true);
+    }
+}
