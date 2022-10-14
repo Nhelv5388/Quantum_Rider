2851c1768b5aaea245a5922ef0eb42105dbd1a0f
sekiryoma <man>
2022-10-14 13:41:47 +0900

    パワーの調整

--- Quantum_Rider/Assets/Member/Seki/Scripts/PlayerMove.cs
+++ Quantum_Rider/Assets/Member/Seki/Scripts/PlayerMove.cs
@@ -2,28 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Uku : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
     [SerializeField]
-    float hoverMaxX,hoverMaxY = 0,hoverPower=1;
+    float hoverMaxX, hoverMaxY = 0, hoverPower = 1;
     [SerializeField]
     GameObject rotationObject;//マウスクリックに応じて角度が変わるオブジェクト
     Quaternion _Rotation;
 
     bool _Pressed = true;
     const int _Power = 200;
-    Vector3 _Myvelocity,_HoverVec, _HoverDirection;
-        
-        // Start is called before the first frame update
+    Vector3 _Myvelocity, _HoverVec, _HoverDirection;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0))
         {//左クリック
             if (_Pressed)
             {
@@ -34,13 +34,13 @@ public class Uku : MonoBehaviour
                 Hover();
             }
         }
-        else if(Input.GetMouseButtonUp(0))
+        else if (Input.GetMouseButtonUp(0))
         {//離したらビーム非表示
-            _Pressed=true;
-            rotationObject.SetActive(false); 
+            _Pressed = true;
+            rotationObject.SetActive(false);
         }
     }
-    
+
     void Direction()
     {
         //進む方向決め
@@ -54,23 +54,31 @@ public class Uku : MonoBehaviour
         //ビーム表示、ビーム方向
         rotationObject.SetActive(true);
         rotationObject.transform.localRotation = _Rotation;
+
+        Invoke("BeamActiveFalse", 0.1f);
+
+        
+    }
+    void BeamActiveFalse()
+    {
+        rotationObject.SetActive(false);
     }
     private void Hover()
     {//浮く
         this.gameObject.GetComponent<Rigidbody2D>().AddForce
         (_HoverDirection * hoverPower * _Power);
-        
+
         VelocityApply();
 
-        if (_Myvelocity.x>hoverMaxX)
+        if (_Myvelocity.x > hoverMaxX)
         {
             _Myvelocity.x = hoverMaxX;
         }
-        else if(_Myvelocity.x<-hoverMaxX)
+        else if (_Myvelocity.x < -hoverMaxX)
         {
             _Myvelocity.x = -hoverMaxX;
         }
-        if(_Myvelocity.y > hoverMaxY)
+        if (_Myvelocity.y > hoverMaxY)
         {
             _Myvelocity.y = hoverMaxY;
         }
@@ -80,7 +88,7 @@ public class Uku : MonoBehaviour
         }
         this.gameObject.GetComponent<Rigidbody2D>().velocity = _Myvelocity;
     }
-    void  VelocityApply()
+    void VelocityApply()
     {
         _Myvelocity = this.gameObject.GetComponent<Rigidbody2D>().velocity;
     }
