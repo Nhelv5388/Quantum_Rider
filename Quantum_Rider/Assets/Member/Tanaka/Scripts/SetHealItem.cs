d03cd1d1169944180926fbf6ad435e3404d125f8
Tanaka-Takehito <21d3170013tt@edu.tech.ac.jp>
2022-10-14 17:32:42 +0900

    ダメージ床(仮画像)

---
+++ Quantum_Rider/Assets/Member/Tanaka/Scripts/SetHealItem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetHealItem : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        
+    }
+}
