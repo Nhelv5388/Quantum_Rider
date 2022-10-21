5f97941a08406331ba58019c9abc7bb9e35f8777
Tanaka-Takehito <21d3170013tt@edu.tech.ac.jp>
2022-10-21 10:52:07 +0900

    ギミックの仕様変更、回復アイテムの実装

---
+++ Quantum_Rider/Assets/Member/Tanaka/Scripts/ColDamageFloor.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColDamageFloor : MonoBehaviour
+{
+    private float nowTime = 0.0f;
+    [SerializeField]
+    private float damageCoolTime = 1.0f;
+
+    private bool startCol = false;
+    private bool endCol = false;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+        if (startCol)
+        {
+            nowTime += Time.deltaTime;
+            //1秒以上ダメージ床に触れていたら1ダメージ
+            if (nowTime >= damageCoolTime)
+            {
+                nowTime = 0.0f;
+                Debug.Log("ダメージを受けました。");
+            }
+            if (endCol)
+            {
+                startCol = false;
+                endCol = false;
+                nowTime = 0.0f;
+            }
+        }
+
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            startCol = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            endCol = true;
+        }
+    }
+
+}
