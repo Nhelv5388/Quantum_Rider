8a29c17e43dd91185c503d897beb4608f41432c0
Tanaka-Takehito <21d3170013tt@edu.tech.ac.jp>
2022-11-11 15:13:13 +0900

    突撃する敵の挙動修正

---
+++ Quantum_Rider/Assets/Member/Tanaka/Scripts/ChargeEnemy/SuvePlayerSerch.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuvePlayerSerch : MonoBehaviour
+{
+    //ƒvƒŒƒCƒ„[‚ª”ÍˆÍ“à‚É“ü‚Á‚½‚©?
+    public bool foundPlayer = false;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            foundPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            foundPlayer = false;
+        }
+    }
+}
