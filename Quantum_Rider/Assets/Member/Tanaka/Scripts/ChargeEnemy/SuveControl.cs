99c449a3e53bf55cb95ad7def3bda8aa323a6357
Tanaka-Takehito <21d3170013tt@edu.tech.ac.jp>
2022-12-02 17:25:34 +0900

    突撃する敵のバグ修正(新規バグ発生中)

--- Quantum_Rider/Assets/Member/Tanaka/Scripts/ChargeEnemy/SuveControl.cs
+++ Quantum_Rider/Assets/Member/Tanaka/Scripts/ChargeEnemy/SuveControl.cs
@@ -21,7 +21,6 @@ public class SuveControl : MonoBehaviour
     private float nowTime = 0;
 
     //進行方向
-    private bool RetrunNow = false;
     public float direction = default;
 
     //プレイヤー、床の両端を計算
@@ -29,26 +28,27 @@ public class SuveControl : MonoBehaviour
     private float floorLeftEdge = default;
     private float enemyRightEdge = default;
     private float enemyLeftEdge = default;
+    private bool onFloor = false;
+    private bool stopping = false;
 
     [SerializeField]
     private GameObject playerSerch;
     [SerializeField]
     private GameObject AttackArea;
 
-    [Header("以下デバッグ用(不死)")]
+    [Header("以下デバッグ用\n(不死)")]
     [SerializeField]
     private bool immortality = false;
 
-
     void Start()
     {
+        //初期化
         suvePlayerSerch = GetComponentInChildren<SuvePlayerSerch>();
         direction = this.gameObject.transform.localRotation.y;
         playerSerch.transform.localPosition = new Vector3(4.0f, 1.0f, 0.0f);
         AttackArea.transform.localPosition = Vector3.zero;
     }
 
-    // Update is called once per frame
     void Update()
     {
         //攻撃のクールタイム中は移動速度を変化させない
@@ -73,7 +73,41 @@ public class SuveControl : MonoBehaviour
                 PatrolEnemy();
             }
         }
+        //バグ対策
+        if (stopping)
+        {
+            stopping = false;
+        }
+        //空中で動かないように
+        if (onFloor)
+        {
+            FloorEdgeReturn();
+        }
+        //判別ようにアタッチしたRB対策
+        playerSerch.transform.localPosition = new Vector3(4.0f,1.0f,0.0f);
+        AttackArea.transform.localPosition = Vector3.zero;
+    }
 
+    private void PatrolEnemy()
+    {
+        if (!stopping)
+        {
+            //SerializeFieldで操作しやすいように100で割る
+            this.transform.Translate(PatrolSpeed / 100, 0, 0);
+        }
+        
+    }
+    private void ChargeEnemy()
+    {
+        if (!stopping)
+        {
+            //SerializeFieldで操作しやすいように100で割る
+            this.transform.Translate(ChargeSpeed / 100, 0, 0);
+        }
+        
+    }
+    private void FloorEdgeReturn()
+    {
         //プレイヤーの座標の両端を計算
         enemyRightEdge = this.gameObject.transform.position.x + (this.gameObject.transform.localScale.x / 2);
         enemyLeftEdge = this.gameObject.transform.position.x - (this.gameObject.transform.localScale.x / 2);
@@ -92,24 +126,7 @@ public class SuveControl : MonoBehaviour
                 this.gameObject.transform.position.z);
             ReturnEnemy();
         }
-
-        playerSerch.transform.localPosition = new Vector3(4.0f,1.0f,0.0f);
-        AttackArea.transform.localPosition = Vector3.zero;
     }
-
-    private void PatrolEnemy()
-    {
-        //SerializeFieldで操作しやすいように100で割る
-        this.transform.Translate(PatrolSpeed/100, 0, 0);
-        
-    }
-    private void ChargeEnemy()
-    {
-        //SerializeFieldで操作しやすいように100で割る
-        this.transform.Translate(ChargeSpeed/100, 0, 0);
-        
-    }
-
     private void ReturnEnemy()
     {    
         //右に進む
@@ -117,14 +134,12 @@ public class SuveControl : MonoBehaviour
         {
             direction = 0;
             this.gameObject.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
-            RetrunNow = false;
         }
         //左に進む
         else if (direction == 0)
         {
             direction = 1;
             this.gameObject.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
-            RetrunNow = true;
         }
     }
     private void ChargeHit(Collision2D col)
@@ -135,12 +150,12 @@ public class SuveControl : MonoBehaviour
             startCoolDown = true;
         }
     }
-
     private void OnCollisionEnter2D(Collision2D col)
     {
-        Debug.Log(col.gameObject.tag);
+        //Debug.Log(col.gameObject.tag);
         if (col.gameObject.tag == "Floor")
         {
+            onFloor = true;
             //現在の床の両端を計算する
             floorRightEdge = col.gameObject.transform.position.x + (col.gameObject.transform.localScale.x / 2);
             floorLeftEdge = col.gameObject.transform.position.x - (col.gameObject.transform.localScale.x / 2);
@@ -149,14 +164,42 @@ public class SuveControl : MonoBehaviour
         {
             ReturnEnemy();
         }
+        if (col.gameObject.tag == "Enemy")
+        {
+            ReturnEnemy();
+        }
         if (col.gameObject.tag == "Player")
         {
             ChargeHit(col);
         }
+        
     }
-
+    private void OnCollisionStay2D(Collision2D col)
+    {
+        //バグ対策
+        if(col.gameObject.tag == "Enemy")
+        {
+            if(col.gameObject.transform.position.x <= this.gameObject.transform.position.x)
+            {
+                stopping = true;
+            }
+            if (!stopping)
+            {
+                ReturnEnemy();
+            }
+        }
+    }
+    private void OnCollisionExit2D(Collision col)
+    {
+        //初期地点に床がない＆床から落ちてしまった時用
+        if(col.gameObject.tag == "Floor")
+        {
+            onFloor = false;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D col)
     {
+        //プレイヤーの攻撃で死亡
         if (!immortality)
         {
             if (col.gameObject.tag == "PlayerAttack")
