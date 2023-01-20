6bcd4ac98833e6676883ca14e72f5abcea2e044c
Karastriker <83107946+Karastriker@users.noreply.github.com>
2023-01-20 11:56:39 +0900

    SceneDiff

--- Quantum_Rider/Assets/Member/Leonard/TitleScreen/SceneChange.cs
+++ Quantum_Rider/Assets/Member/Leonard/TitleScreen/SceneChange.cs
@@ -5,11 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    [SerializeField] private MapManager.SceneID mapName;
+    //[SerializeField] private MapManager.SceneID mapName;
     public void MoveToScene()
     {
         //Debug.Log("MoveToScene");
-        MapManager.Instance.CallFadeIn(mapName);
-        //SceneManager.LoadScene("MainGameScene");
+        //MapManager.Instance.CallFadeIn(mapName);
+        SceneManager.LoadScene("Difficulty");
+    }
+
+    public void exitgame()
+    {
+        Debug.Log("ExitGame");
+        Application.Quit();
+    }
+
+    public void EasyMode()
+    {
+        Debug.Log("Easy");
+        SceneManager.LoadScene("Easy");
+    }
+
+    public void HardMode()
+    {
+        Debug.Log("Hard");
+        SceneManager.LoadScene("Hard");
     }
 }
